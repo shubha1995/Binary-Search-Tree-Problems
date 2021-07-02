8f6bdbd9f63559a625192d2d3d08e190160322bc
shubhasankar <ssbhaumikdmr@gmail.com>
2021-07-02 13:33:31 +0530

    Added: Create_a_BST

--- src/com/hashtable/BinaryTree.java
+++ src/com/hashtable/BinaryTree.java
@@ -1,6 +1,49 @@
 package com.hashtable;
 
+
+class Node{
+	int data;
+	Node left;
+	Node right;
+
+}
+
 public class BinaryTree {
 
+
+	public Node insert(Node root, int val) {
+		if (root == null) {
+			return craeteNewNode(val);
+		}
+		if (val < root.data) {
+			root.left = insert(root.left, val);
+		}
+		else if (val > root.data) {
+			root.right = insert(root.right, val);
+		}
+		return root;
+
+	}
+
+
+	public Node craeteNewNode(int k) {
+		Node bst = new Node();
+		bst.data = k;
+		bst.left = null;
+		bst.right = null;
+		return bst;
+	}
+
+
+	public void print(Node root) {
+		if (root == null) {
+			return;
+		}
+		print(root.left);
+		System.out.println(root.data);
+		print(root.right);
+		
+		
+	}
 
 }
--- src/com/hashtable/MainBst.java
+++ src/com/hashtable/MainBst.java
@@ -3,6 +3,19 @@ package com.hashtable;
 public class MainBst {
 	public static void main(String[] args) {
 		System.out.println("Welcome to BST problem");
+		
+		BinaryTree bst = new BinaryTree();
+		Node root = null;
+		
+		root = bst.insert(root, 56);
+		
+		root = bst.insert(root, 30);
+	
+		//30->56->70->80
+		root = bst.insert(root, 70);
+
+		bst.print(root);
+
 	}
 
 }
